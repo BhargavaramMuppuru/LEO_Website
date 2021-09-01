48ba93846e41e062e06c813a4759e6d5e5c5c452
Chebrolu Tejaswi <68593617+ChebroluTejaswi@users.noreply.github.com>
2021-09-01 12:11:11 +0530

    Update urls.py

--- cis_website/urls.py
+++ cis_website/urls.py
@@ -4,13 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-    path('events', views.about, name='events'),
+    path('events', views.events, name='events'),
     path('blog', views.blog, name='blog'),
-     path('boards', views.blog, name='boards'),
+    path('board', views.board, name='board'),
+    path('boards', views.blog, name='boards'),
     path('contact_us', views.contact, name='contact'),
     path('upload_blog', views.upload_blog, name='upload_blog'),
     path('robots.txt', views.robots),
     path('sitemap.xml', views.sitemap),
-    
-    
 ]
